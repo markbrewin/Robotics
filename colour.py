23484550925ed1ac2eb55f26d8f24637c2d9538e
Mark <mark.brewin98@gmail.com>
2019-03-05 23:12:16 +0000

    Add files via upload

--- colour.py
+++ colour.py
@@ -8,26 +8,41 @@ Created on Mon Feb 25 15:15:44 2019
 import rospy
 import cv2
 import numpy
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image, LaserScan
 from std_msgs.msg import String
 from geometry_msgs.msg import Twist
 from cv_bridge import CvBridge, CvBridgeError
 
 
-class colour:
+class colourSearch:
 
     def __init__(self):
-
         cv2.startWindowThread()
         self.bridge = CvBridge()
-        self.image_sub = rospy.Subscriber("/camera/rgb/image_raw",
-                                          Image, self.callback)
+        
+        self.subImage = rospy.Subscriber("/camera/rgb/image_raw", Image, self.callbackImage)
+        self.subScan = rospy.Subscriber("/scan", LaserScan, self.callbackScan)
                                           
-        self.cmd_vel_pub = rospy.Publisher('/mobile_base/commands/velocity', Twist, queue_size=1)
+        self.pubCmdVel = rospy.Publisher('/mobile_base/commands/velocity', Twist, queue_size=1)
+        
         self.twist = Twist()
+        
+        self.distance = 0        
+        self.found = [False, False, False, False]
+        
+        self.threshYelLow = numpy.array((20, 100, 100))
+        self.threshYelHigh = numpy.array((30, 255, 255))
+        
+        self.threshBluLow = numpy.array((110, 50, 50))
+        self.threshBluHigh = numpy.array((130, 255, 255))
+        
+        self.threshRedLow = numpy.array((0, 100, 100))
+        self.threshRedHigh = numpy.array((10, 255, 255))
+        
+        self.threshGreLow = numpy.array((50, 100, 100))
+        self.threshGreHigh = numpy.array((70, 255, 255))
 
-    def callback(self, data):
-
+    def callbackImage(self, data):
         cv2.namedWindow("Image window", 1)
         
         try:
@@ -37,29 +52,16 @@ class colour:
 
         imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
         
-        threshYelLow = numpy.array((20, 100, 100))
-        threshYelHigh = numpy.array((30, 255, 255))
-        
-        threshBluLow = numpy.array((110, 50, 50))
-        threshBluHigh = numpy.array((130, 255, 255))
-        
-        threshRedLow = numpy.array((0, 100, 100))
-        threshRedHigh = numpy.array((10, 255, 255))
-        
-        threshGreLow = numpy.array((36, 25, 25))
-        threshGreHigh = numpy.array((70, 255, 255))
-        
-        maskYellow = cv2.inRange(imgHSV, threshYelLow, threshYelHigh)
-        maskBlue = cv2.inRange(imgHSV, threshBluLow, threshBluHigh)
-        maskRed = cv2.inRange(imgHSV, threshRedLow, threshRedHigh)
-        maskGreen = cv2.inRange(imgHSV, threshGreLow, threshGreHigh)
+        maskYellow = cv2.inRange(imgHSV, self.threshYelLow, self.threshYelHigh)
+        maskBlue = cv2.inRange(imgHSV, self.threshBluLow, self.threshBluHigh)
+        maskRed = cv2.inRange(imgHSV, self.threshRedLow, self.threshRedHigh)
+        maskGreen = cv2.inRange(imgHSV, self.threshGreLow, self.threshGreHigh)
 
         h, w, d = img.shape
         
         momentsAll = cv2.moments(maskYellow + maskBlue + maskRed + maskGreen)
         
         if momentsAll['m00'] > 0:
-            
             for colID in range(4):
                 if colID == 0:
                     moments = cv2.moments(maskYellow)
@@ -74,30 +76,37 @@ class colour:
                     moments = cv2.moments(maskGreen)
                     colour = "Green"
                 
-                if moments['m00'] > 0:
+                if moments['m00'] > 0 and self.found[colID] == False and self.distance > 1:                           
+                    cx = int(moments['m10']/moments['m00'])
+                    cy = int(moments['m01']/moments['m00'])
+                    
+                    cv2.circle(img, (cx, cy), 20, (0, 0, 255), -1)
+                    
+                    err = cx - w/2
+                    
+                    self.twist.linear.x = 0.2
+                    self.twist.angular.z = -float(err) / 100
+                
+                    self.pubCmdVel.publish(self.twist)
+                    
                     break
-            
-            print(colour + " found. (" + str(colID) + ")")            
-            
-            cx = int(moments['m10']/moments['m00'])
-            cy = int(moments['m01']/moments['m00'])
-            
-            cv2.circle(img, (cx, cy), 20, (0, 0, 255), -1)
-            
-            err = cx - w/2
-            
-            self.twist.linear.x = 0.2
-            self.twist.angular.z = -float(err) / 100
+                
+                elif moments['m00'] > 0 and self.found[colID] == False and self.distance <= 1:
+                    print(colour + " found. (" + str(colID) + ")")
+                    self.found[colID] = True
+                    print self.found
         
-            print self.twist.angular.z
-
-            self.cmd_vel_pub.publish(self.twist)
+                    break
             
         cv2.imshow("Image window", img)
         cv2.waitKey(1)
+        
+    def callbackScan(self, data):   
+        self.distance = data.ranges[len(data.ranges)/2]        
+        print("Distance: " + str(self.distance))
 
 
-colour()
-rospy.init_node('colour', anonymous=True)
+colourSearch()
+rospy.init_node('colourSearch', anonymous=True)
 rospy.spin()
 cv2.destroyAllWindows()
